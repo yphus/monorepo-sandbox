1747ecfe451c279c587a7d76da0e291b22553166
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-03 16:01:30 +0000

    Adding tarfile member sanitization to extractall()

--- checkbox-ng/checkbox_ng/launcher/merge_reports.py
+++ checkbox-ng/checkbox_ng/launcher/merge_reports.py
@@ -51,7 +51,26 @@ class MergeReports():
     def _parse_submission(self, submission, tmpdir, mode="list"):
         try:
             with tarfile.open(submission) as tar:
-                tar.extractall(tmpdir.name)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, tmpdir.name)
                 with open(os.path.join(
                           tmpdir.name, 'submission.json')) as f:
                     data = json.load(f)
--- checkbox-ng/checkbox_ng/launcher/merge_submissions.py
+++ checkbox-ng/checkbox_ng/launcher/merge_submissions.py
@@ -58,7 +58,29 @@ class MergeSubmissions(MergeReports):
         with open(ctx.args.output_file, 'wb') as stream:
             exporter.dump_from_session_manager(manager, stream)
         with tarfile.open(ctx.args.output_file) as tar:
-            tar.extractall(tmpdir.name)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, tmpdir.name)
         with tarfile.open(ctx.args.output_file, mode='w:xz') as tar:
             tar.add(tmpdir.name, arcname='')
         print(ctx.args.output_file)
